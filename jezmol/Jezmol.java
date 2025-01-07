e9d699d4f65cbac58e3e05ad376e58f5ed61ac15
khalil chermiti <khalil.chermiti@proton.me>
2025-01-07 17:56:16 +0100

    chore: finish writing jezmol tokenizer

--- jezmol/Jezmol.java
+++ jezmol/Jezmol.java
@@ -1,9 +1,7 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.ArrayList;
-import java.io.IOException;
 
 sealed interface Token {
   record LeftBrace() implements Token {
@@ -33,7 +31,7 @@ sealed interface Token {
   record BooleanToken(Boolean value) implements Token {
   }
 
-  record Number(Long value) implements Token {
+  record Number(Double value) implements Token {
   }
 }
 
@@ -69,8 +67,9 @@ public class Jezmol {
     try {
       String content = Files.readString(Path.of(args[0]));
       tokenize(content);
+      tokens.stream().forEach(token -> System.out.println("Token : " + token.getClass().getSimpleName()));
     } catch (Exception e) {
-      System.out.println("Error reading file: " + e.getMessage());
+      System.out.println(e.getMessage());
     }
   }
 
@@ -78,34 +77,96 @@ public class Jezmol {
     int current = 0;
 
     while (current < input.length()) {
-      char currentChar = input.charAt(current);
+      char currentChar = safeCharAt(input, current++);
 
       switch (currentChar) {
-        case ('{') -> {
-        }
-        case ('}') -> {
-        }
-        case ('[') -> {
-        }
-        case (']') -> {
-        }
-        case ('"') -> {
-        }
-        case '-' | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' -> {
-        }
-        case (',') -> {
-        }
-        case (':') -> {
-        }
+        case ('{') -> tokens.add(new Token.LeftBrace());
+        case ('}') -> tokens.add(new Token.RightBrace());
+        case ('[') -> tokens.add(new Token.LeftBracket());
+        case (']') -> tokens.add(new Token.RightBracket());
+        case (',') -> tokens.add(new Token.Comma());
+        case (':') -> tokens.add(new Token.Column());
+
         case ('t') -> {
+          if (safeLookahead(input, current, 3).equals("rue")) {
+            tokens.add(new Token.BooleanToken(true));
+            current += 3;
+          } else {
+            throw new Exception("invalid token when parsing boolean token");
+          }
         }
+
         case ('f') -> {
+          if (safeLookahead(input, current, 4).equals("alse")) {
+            tokens.add(new Token.BooleanToken(false));
+            current += 4;
+          } else {
+            throw new Exception("invalid token when parsing boolean token");
+          }
         }
+
         case ('n') -> {
+          if (safeLookahead(input, current, 3).equals("ull")) {
+            tokens.add(new Token.Null());
+            current += 3;
+          } else {
+            throw new Exception("invalid token when parsing null token");
+          }
         }
+
+        case ('"') -> {
+          StringBuilder string = new StringBuilder();
+          while (safeCharAt(input, current) != '"') {
+            string.append(safeCharAt(input, current));
+            current++;
+          }
+          tokens.add(new Token.StringToken(string.toString()));
+          current++; // consume closing quote
+        }
+
         default -> {
+          if (Character.isWhitespace(currentChar)) {
+            continue;
+          }
+
+          if (Character.isDigit(currentChar) || currentChar == '-') {
+            StringBuilder number = new StringBuilder();
+            number.append(currentChar);
+
+            // edge case : -.6
+            if (currentChar == '-' && !Character.isDigit(safeCharAt(input, current)))
+              throw new Exception("Invalid number format, expect digit after: -");
+
+            while (Character.isDigit(safeCharAt(input, current)) || safeCharAt(input, current) == '.') {
+              number.append(safeCharAt(input, current));
+              current++;
+            }
+
+            // edge case : 543.
+            if (safeCharAt(input, current - 1) == '.')
+              throw new Exception("Invalid number format, number can't end with: .");
+            tokens.add(new Token.Number(Double.parseDouble(number.toString())));
+
+            continue;
+          }
+
+          throw new Exception("Invalid token: " + currentChar);
         }
       }
     }
+  }
+
+  static char safeCharAt(String input, int index) throws Exception {
+    if (index >= input.length()) {
+      throw new Exception("Unexpected end of input while accessing character.");
+    }
+    return input.charAt(index);
+  }
+
+  static String safeLookahead(String input, int current, int lookaheadLength) throws Exception {
+    if (current + lookaheadLength > input.length()) {
+      throw new Exception("Unexpected end of input while looking ahead.");
+    }
+    return input.substring(current, current + lookaheadLength);
   }
 }
